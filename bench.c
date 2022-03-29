ecc33c8c2dc929d31431f66acba69c624a0e6f87
Sebastian Ramacher <Sebastian.Ramacher@ait.ac.at>
2022-03-29 11:54:06 +0200

    Extend benchmarks with serialization

--- bench.c
+++ bench.c
@@ -54,11 +54,13 @@ static void bench_tbfe(void) {
   tbfe_bbg_secret_key_t sk;
   tbfe_bbg_public_key_t pk;
 
+  /* benchmark key generation */
   tbfe_bbg_init_public_key(&pk, total_depth);
   tbfe_bbg_init_secret_key(&sk, bloom_filter_size, cellsize, number_hash_functions);
   BENCH_ONCE("tbfe keygen",
              tbfe_bbg_keygen(&pk, &sk, bloom_filter_size, number_hash_functions, total_levels));
 
+  /* benchmark encaps */
   BENCH_BEGIN("tbfe encaps") {
     uint8_t K[SECURITY_PARAMETER];
     tbfe_bbg_ciphertext_t ciphertext;
@@ -67,6 +69,23 @@ static void bench_tbfe(void) {
     tbfe_bbg_clear_ciphertext(&ciphertext);
   }
   BENCH_END;
+  /* benchmark encaps + serialization */
+  BENCH_BEGIN("tbfe encaps + serialization") {
+    uint8_t K[SECURITY_PARAMETER];
+    tbfe_bbg_ciphertext_t ciphertext;
+    uint8_t* serialized_ciphertext = NULL;
+    tbfe_bbg_init_ciphertext(&ciphertext);
+    BENCH_ADD(do {
+      tbfe_bbg_encaps(K, &ciphertext, &pk, 1);
+      serialized_ciphertext = malloc(tbfe_bbg_get_ciphertext_size(&ciphertext));
+      tbfe_bbg_serialize_ciphertext(serialized_ciphertext, &ciphertext);
+    } while (0));
+    free(serialized_ciphertext);
+    tbfe_bbg_clear_ciphertext(&ciphertext);
+  }
+  BENCH_END;
+
+  /* benchmark decaps */
   BENCH_BEGIN("tbfe decaps") {
     uint8_t K[SECURITY_PARAMETER], Kd[SECURITY_PARAMETER];
     tbfe_bbg_ciphertext_t ciphertext;
@@ -74,6 +93,23 @@ static void bench_tbfe(void) {
     tbfe_bbg_encaps(K, &ciphertext, &pk, 1);
     BENCH_ADD(tbfe_bbg_decaps(Kd, &ciphertext, &sk, &pk));
     tbfe_bbg_clear_ciphertext(&ciphertext);
+  }
+  BENCH_END;
+  BENCH_BEGIN("tbfe decaps + serialization") {
+    uint8_t K[SECURITY_PARAMETER], Kd[SECURITY_PARAMETER];
+    tbfe_bbg_ciphertext_t ciphertext;
+    tbfe_bbg_init_ciphertext(&ciphertext);
+    tbfe_bbg_encaps(K, &ciphertext, &pk, 1);
+    uint8_t* serialized_ciphertext = malloc(tbfe_bbg_get_ciphertext_size(&ciphertext));
+    tbfe_bbg_serialize_ciphertext(serialized_ciphertext, &ciphertext);
+    tbfe_bbg_clear_ciphertext(&ciphertext);
+
+    BENCH_ADD(do {
+      tbfe_bbg_init_ciphertext_from_serialized(&ciphertext, serialized_ciphertext);
+      tbfe_bbg_decaps(Kd, &ciphertext, &sk, &pk);
+    } while (0));
+    tbfe_bbg_clear_ciphertext(&ciphertext);
+    free(serialized_ciphertext);
   }
   BENCH_END;
 
